b820002527d8aaa2c65c44b31c4a070798ff9647
AlanGonzalez10123 <alangonzalezosorio04@gmail.com>
2025-03-29 14:25:25 -0400

    Cleaned up init.py

--- backend/init.py
+++ backend/init.py
@@ -4,22 +4,30 @@ from dotenv import load_dotenv
 import deepl
 import whisper
 
-load_dotenv()
-genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+load_dotenv()#load .env
 
+#Gemini Config
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 model = genai.GenerativeModel("gemini-2.0-flash")
+
+#DeepL Config
+auth_key = os.environ["DEEPL_API_KEY"]
+translator = deepl.Translator(auth_key)
+
+#OpenAI Whisper Config
+whisper_model = whisper.load_model("tiny")
+
+"""
+Testing all configs
+"""
 
 response = model.generate_content("Generate one sentence about cats")
 gemini_response = response.text
 
 print("English Sentence: ", gemini_response)
 
-
-auth_key = os.environ["DEEPL_API_KEY"]
-translator = deepl.Translator(auth_key)
 result=translator.translate_text(gemini_response, target_lang="ES")
 print("To Spanish Translation: ", result.text)
 
-whisper_model = whisper.load_model("tiny")
 result = whisper_model.transcribe("spanish.mp3", fp16=False)
 print(result["text"])
